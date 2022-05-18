24380d33887f52fd50cf65cc4f231377d9000000
Ameen Al-Kaisi <ameen.alkaisi01@gmail.com>
2022-05-18 03:36:30 +0300

    made it possible for dwm's status bar's while loop to exit when logging out

--- .dwm/status-bar.sh
+++ .dwm/status-bar.sh
@@ -1,5 +1,25 @@
-while true;
+#!/bin/bash
+get_vol () {
+    actual_vol=$(amixer sget Master | awk -F"[][]" '/dB/ { printf "%d", $2 }');
+    cur_vol=$(expr $actual_vol / 10);
+    output=""
+    for i in {1..10}; do
+        if [[ $i -le $cur_vol ]] ; then
+            output+=":";
+        else
+            output+="-";
+        fi
+    done
+
+    output+=" ";
+    output+="$actual_vol";
+    output+="%";
+    echo $output;
+}
+
+while [[ $GDMSESSION == "dwm" && $(who | grep -w $USER | wc -l) -eq 1 ]]
 do 
-    xsetroot -name "$(acpitool -b | awk '{ print $5 }') | $(date)";
+    xsetroot -name "$(get_vol) | $(acpitool -b | awk '{ print $5 }') | $(date)";
+    #xsetroot -name "testing!";
     sleep 1s;
 done &
--- README.md
+++ README.md
@@ -3,6 +3,3 @@ This repo contains general stuff I use in Linux
 
 ## for vim
 create ~/.vim/colors directories, and get candycolor from google then put it in that directory
-
-## for dwm
-I'm not proud of the infinite while loop in the status bar script, but it seems to be the community verified way to set a status bar 
