b97a87f9f349a997e141bef8eaff6031626bdb16
Ameen Al-Kaisi <ameen.alkaisi01@gmail.com>
2022-12-23 18:33:55 -0500

    updated nvim-windows

---
+++ nvim-windows/lua/global/set.lua
@@ -0,0 +1,31 @@
+vim.opt.foldmethod = 'expr'
+vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+
+vim.opt.guicursor = "n-c-v:block-nC"
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.undodir = "C:/Users/ameen/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "0"
