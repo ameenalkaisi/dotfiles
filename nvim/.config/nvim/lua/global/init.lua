3444175965eb5ec8af6347cafe5b093d37f4bf60
Ameen Al-Kaisi <ameen.alkaisi01@gmail.com>
2023-02-09 17:52:46 -0500

    removed redundant settings that were moved earlier

--- nvim/.config/nvim/lua/global/init.lua
+++ nvim/.config/nvim/lua/global/init.lua
@@ -16,26 +16,3 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup('plugins')
-
-local augroup = vim.api.nvim_create_augroup
-local fix_spaces_group = augroup('FixSpaces', {})
-
-local autocmd = vim.api.nvim_create_autocmd
-local yank_group = augroup('HighlightYank', {})
-
-autocmd('TextYankPost', {
-    group = yank_group,
-    pattern = '*',
-    callback = function()
-        vim.highlight.on_yank({
-            higroup = 'IncSearch',
-            timeout = 40,
-        })
-    end,
-})
-
-autocmd({ "BufWritePre" }, {
-    group = fix_spaces_group,
-    pattern = "*",
-    command = [[%s/\s\+$//e]],
-})
