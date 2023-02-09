e2d039d1f67382dfbbf1896569512efd24d5d98c
Ameen Al-Kaisi <ameen.alkaisi01@gmail.com>
2023-02-09 18:11:00 -0500

    fixed issue where on startup nvim-tree was not running if it was an empty dir

--- nvim/.config/nvim/lua/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -7,6 +7,23 @@ return {
             },
         })
 
+        vim.api.nvim_create_autocmd({ "VimEnter" }, {
+            callback = function(data)
+                -- buffer is a directory
+                local directory = vim.fn.isdirectory(data.file) == 1
+
+                if not directory then
+                    return
+                end
+
+                -- change to the directory
+                vim.cmd.cd(data.file)
+
+                -- open the tree
+                require("nvim-tree.api").tree.open()
+            end
+        })
+
         vim.keymap.set("n", "<leader>nt", vim.cmd.NvimTreeToggle)
         vim.keymap.set("n", "<leader>nf", vim.cmd.NvimTreeFindFile)
     end
