e80e27378a41d3beb659bd36e3325fc491c06f92
Ameen Al-Kaisi <ameen.alkaisi01@gmail.com>
2023-02-10 01:19:10 -0500

    set up null ls along with automatic mason setup

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -28,6 +28,29 @@ return {
             library = { plugins = { "nvim-dap-ui" }, types = true }
         })
         require("mason").setup()
+        local mason_null_ls = require("mason-null-ls")
+        mason_null_ls.setup({
+            ensure_installed = {
+
+            },
+            automatic_installation = false,
+            automatic_setup = true,
+        })
+        require("null-ls").setup()
+        mason_null_ls.setup_handlers({
+            function(source_name, methods)
+                -- all sources with no handler get passed here
+
+                -- To keep the original functionality of `automatic_setup = true`,
+                -- please add the below.
+                require("mason-null-ls.automatic_setup")(source_name, methods)
+            end,
+            -- example specific config
+            -- stylua = function(source_name, methods)
+            --     null_ls.register(null_ls.builtins.formatting.stylua)
+            -- end,
+        })
+
         require("mason-lspconfig").setup()
 
         vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
