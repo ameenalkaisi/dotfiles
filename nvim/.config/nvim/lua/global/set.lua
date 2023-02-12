f552d3b8dacb61c5a647093032a4c8de6b85f6a1
Ameen Al-Kaisi <ameen.alkaisi01@gmail.com>
2023-02-11 22:47:40 -0500

    re-enabled netrw since it allows gx functionality, and it doesn't seem to affect nvim-tree that much

--- nvim/.config/nvim/lua/global/set.lua
+++ nvim/.config/nvim/lua/global/set.lua
@@ -1,8 +1,8 @@
 -- vim.g.netrw_browse_split = 0
 -- vim.g.netrw_banner = 0
 -- vim.g.netrw_winsize = 25
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+-- vim.g.loaded_netrw = 1
+-- vim.g.loaded_netrwPlugin = 1
 
 vim.opt.foldmethod = 'expr'
 vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
