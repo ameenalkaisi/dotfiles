19b582eac7c834877c1fc09c1c1b00151efd286f
Ameen Al-Kaisi <ameen.alkaisi@borderconnect.com>
2024-05-23 12:01:31 -0400

    Add descriptions to trouble menu

--- nvim/.config/nvim/lua/plugins/trouble.lua
+++ nvim/.config/nvim/lua/plugins/trouble.lua
@@ -2,28 +2,26 @@ return {
     "folke/trouble.nvim",
     dependencies = "nvim-tree/nvim-web-devicons",
     config = function()
-        local opts = { silent = true, noremap = true }
+        local opts = function(desc)
+            return { silent = true, noremap = true, desc = desc }
+        end
         -- Lua
-        vim.keymap.set("n", "<leader>xx", vim.cmd.TroubleToggle, opts)
-
-        vim.keymap.set("n", "<leader>xw", function()
-            vim.cmd("TroubleToggle document_diagnostics")
-        end, opts)
+        vim.keymap.set("n", "<leader>xx", vim.cmd.TroubleToggle, opts("Toggle trouble menu"))
 
         vim.keymap.set("n", "<leader>xd", function()
             vim.cmd("TroubleToggle document_diagnostics")
-        end, opts)
+        end, opts("Toggle trouble document diagnostics"))
 
         vim.keymap.set("n", "<leader>xl", function()
             vim.cmd("TroubleToggle loclist")
-        end, opts)
+        end, opts("Toggle trouble on local list"))
 
         vim.keymap.set("n", "<leader>xq", function()
             vim.cmd("TroubleToggle quickfix")
-        end, opts)
+        end, opts("Toggle trouble on quick fix list"))
 
         vim.keymap.set("n", "gR", function()
             vim.cmd("TroubleToggle lsp_references")
-        end, opts)
+        end, opts("Toggle trouble on lsp references"))
     end,
 }
