b1048df5b0ba43eb49059f6b074b1cea2bc2aab2
Ameen Al-Kaisi <ameen.alkaisi01@gmail.com>
2024-06-06 19:44:12 -0400

    Fixes to gitignore and themery

--- nvim/.config/nvim/lua/global/utils.lua
+++ nvim/.config/nvim/lua/global/utils.lua
@@ -1,24 +1,10 @@
 M = {}
 
-local function mkdir_p(path)
-    -- Recursively create directories as needed
-    local current = ""
-    for dir in string.gmatch(path, "([^/]+)") do
-        current = current .. dir .. "/"
-        -- Use OS-specific command to create the directory
-        if not os.execute('mkdir "' .. current .. '" 2>nul') and not os.execute('mkdir -p "' .. current .. '"') then
-            print("Error: Could not create directory " .. current)
-            return false
-        end
-    end
-    return true
-end
-
 M.touch_file = function(filename, content)
     -- Extract the directory path
     local path = string.match(filename, "(.*/)")
     if path then
-        mkdir_p(path)
+        os.execute('mkdir -p "' .. path .. '"')
     end
 
     -- Attempt to open the file in read mode
@@ -26,6 +12,7 @@ M.touch_file = function(filename, content)
 
     if file then
         -- File exists, close the file
+        vim.cmd(string.format("source %s", filename))
         file:close()
     else
         -- File doesn't exist, create the file
