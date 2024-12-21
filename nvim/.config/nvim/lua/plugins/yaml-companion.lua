02838e2f8e8899bb30b7e518bb079d8cbe9eab65
Ameen Al-Kaisi <ameen.alkaisi01@gmail.com>
2024-12-21 09:52:21 -0500

    yaml-companion lazy loading, removing duplicate check messages

--- nvim/.config/nvim/lua/plugins/yaml-companion.lua
+++ nvim/.config/nvim/lua/plugins/yaml-companion.lua
@@ -1,6 +1,7 @@
 return {
 	"someone-stole-my-name/yaml-companion.nvim",
-	dependencies = {
+	ft = { "yml", "yaml" },
+	dependencies= {
 		"neovim/nvim-lspconfig",
 		"nvim-lua/plenary.nvim",
 		"nvim-telescope/telescope.nvim",
